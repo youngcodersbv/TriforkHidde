69e4662f1e7b496f51ec19984f1f7105fdbdb513
Hidde Uittenbosch <hidde@Hiddes-MBP.trifork.nl>
2021-10-05 10:22:41 +0200

    first commit

---
+++ src/main/java/com/example/volleybal/VolleybalApplication.java
@@ -0,0 +1,13 @@
+package com.example.volleybal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleybalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleybalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/volleybal/VolleybalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.volleybal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleybalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
